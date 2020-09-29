0ddce4f3479b3a86ab61eb38943269d9bff4ea17
alvaroamoros <100421520@alumnos.uc3m.es>
2020-09-29 17:48:31 +0200

    20/09/20

--- Data Science  Wrangling.R
+++ Data Science  Wrangling.R
@@ -185,3 +185,161 @@ tmp2
 
 # Question 14 - Which function can reshape tmp2 to a table with six rows and five columns named major, admitted_men, admitted_women, applicants_men and applicants_women
 spread(tmp2, column_name, value)
+
+
+    # Combining Tables
+
+      # left_join() only keeps rows that have information in the first table.
+      # right_join() only keeps rows that have information in the second table.
+      # inner_join() only keeps rows that have information in both tables.
+      # full_join() keeps all rows from both tables.
+      # semi_join() keeps the part of first table for which we have information in the second.
+      # anti_join() keeps the elements of the first table for which there is no information in the second
+
+# import US murders data
+library(tidyverse)
+library(ggrepel)
+library(dslabs)
+ds_theme_set()
+data(murders)
+head(murders)
+
+# import US election results data
+data(polls_us_election_2016)
+head(results_us_election_2016)
+identical(results_us_election_2016$state, murders$state)
+
+# join the murders table and US election results table
+tab <- left_join(murders, results_us_election_2016, by = "state")
+tab
+
+tab %>% ggplot(aes(population/10^6, electoral_votes, label = abb)) +
+  geom_point() +
+  geom_text_repel() +
+  scale_x_continuous(trans = "log2") +
+  scale_y_continuous(trans = "log2") +
+  geom_smooth(method = "ln", se =FALSE)
+
+tab1 <- slice(murders, 1:6) %>% select(state, population)
+tab1
+
+tab2 <- slice(results_us_election_2016, c(1:3, 5,  7:8)) %>% select(state, electoral_votes)
+tab2
+
+left_join(tab1, tab2)
+tab1 %>% left_join(tab2)
+tab1 %>% right_join(tab2)
+
+inner_join(tab1 , tab2)
+
+full_join(tab1, tab2)
+
+semi_join(tab1, tab2)
+anti_join(tab1, tab2)
+
+bind_cols(a = 1:3, b = 4:6)
+
+tab1 <- tab[, 1:3]
+tab2 <- tab[, 4:6]
+tab3 <- tab[, 7:9]
+tab1
+tab2
+tab3
+new_tab <- bind_cols(tab1,  tab2, tab3)
+new_tab
+
+
+tab1 <- tab[1:5,]
+tab2 <- tab[6:12,]
+tab1
+tab2
+bind_rows(tab1, tab2)
+
+
+#  intersect vectors or data frames
+intersect(1:10, 5:15)
+intersect(c("a", "b", "c"), c("a", "d", "s"))
+
+tab1 <- tab[1:5,]
+tab2 <- tab[3:6,]
+intersect(tab1, tab2)
+
+# perform a union of vectors or data frames
+union(1:10, 6:15)
+union(c("a", "b", "c"),c("a","d","s"))
+union(tab1, tab2)
+
+# set difference of vectors or data frames
+setdiff(1:10, 5:15)
+setdiff(c("a", "b", "c"),c("a","d","s"))
+setdiff(tab1, tab2)
+
+# setequal determines whether sets have the same elements, regardless of order
+setequal(1:10, 5:15)
+setequal(c("a", "b", "c"),c("a","d","s"))
+setequal(tab1, tab2)
+
+
+    # Assessment: Combining Tables
+tab1 <- slice(murders, 1:6) %>% select(state, population)
+tab2 <- slice(results_us_election_2016, c(1:3,  7:8)) %>% select(state, electoral_votes)
+tab1
+tab2
+dat <- left_join(tab1, tab2, by = "state")
+dat
+
+
+
+install.packages("Lahman")
+library(Lahman)
+
+# Question 5 - Use the correct join or bind function to create a combined table of the names and statistics of the top 10 home run (HR) hitters for 2016. 
+# This table should have the player ID, first name, last name, and number of HR for the top 10 players. Name this data frame top_names.
+
+top <- Batting %>% 
+  filter(yearID == 2016) %>%
+  arrange(desc(HR)) %>%    # arrange by descending HR count
+  slice(1:10)    # take entries 1-10
+top %>% as_tibble()
+top
+
+Master %>% as_tibble()
+
+top_names <- top %>%  left_join(Master)%>%
+  select(playerID, nameFirst, nameLast, HR)
+top_names
+
+# Question 6 Inspect the Salaries data frame. Filter this data frame to the 2016 salaries, 
+#then use the correct bind join function to add a salary column to the top_names data frame from the previous question. 
+#Name the new data frame top_salary. Use this code framework:
+
+top_salary <- Salaries %>% filter(yearID == 2016) %>%
+  right_join(top_names) %>%
+  select(nameFirst, nameLast, teamID, HR, salary)
+top_salary
+
+# QUestion 7 - 
+# Inspect the AwardsPlayers table. Filter awards to include only the year 2016.
+head(AwardsPlayers)
+awards_2016 <- AwardsPlayers %>% filter(yearID == 2016)
+awards_2016
+
+# How many players from the top 10 home run hitters won at least one award in 2016?
+inner_join(top, awards_2016)
+
+# How many players won an award in 2016 but were not one of the top 10 home run hitters in 2016?
+
+awards_2016 <- awards_2016 %>% select(playerID)
+
+top <- top %>% select(playerID)
+
+setdiff(awards_2016, top)
+
+
+    # Web Scraping
+install.packages("rvest")
+library(rvest)
+url <- "https://en.wikipedia.org/wiki/Murder_in_the_United_States_by_state"
+h <- read_html(url)
+class(h)
+h
--- Libraries.R
+++ Libraries.R
@@ -1,44 +1,23 @@
 #Libraries
 
 library(dplyr)
-
-
 library(tidyverse)
-
-
 library(dslabs)
-
-
 library(HistData)
-
-
 library(gtools)
-
-
 library(ggrepel)
-
-
 library(ggplot2)
-
 library(epitools)
-
+library(rvest)
 
 install.packages("dplyr")
-
 install.packages("tidyverse")
-
 install.packages("dslabs")
-
 install.packages("HistData")
-
 install.packages("HistData")
-
 install.packages("gtools")
-
 install.packages("ggrepel")
-
 install.packages("ggplot2")
-
 install.packages("epitools")
-
+install.packages("rvest")
 
