c6c8ae0f0687e2a4e5dc87977b8dc356720870f7
alvaroamoros <100421520@alumnos.uc3m.es>
2020-10-08 10:59:07 +0200

    s

--- Data Science  Wrangling.R
+++ Data Science  Wrangling.R
@@ -147,3 +147,41 @@ dat
         unite(variable_name, first_variable_name, second_variable_name, sep="_") %>%
         spread(variable_name, value) %>%
         rename(fertility = fertility_NA)
+      
+ 
+#Assessment Part 2: Reshaping Data  
+library(tidyverse)
+library(dslabs)
+co2
+head(co2)    
+
+#Question 10 - Run the following command to define the co2_wide object:
+co2_wide <- data.frame(matrix(co2, ncol = 12, byrow = TRUE)) %>% 
+  setNames(1:12) %>%
+  mutate(year = as.character(1959:1997))
+co2_wide
+head(co2_wide)
+
+co2_tidy <- gather(co2_wide, month, co2, -year)
+head(co2_tidy)
+
+# Questio 11 - Use co2_tidy to plot CO2 versus month with a different curve for each year:
+co2_tidy %>% ggplot(aes(as.numeric(month), co2, color = year)) + geom_line()
+
+# Question 12 
+library(dslabs)
+data(admissions)
+dat <- admissions %>% select(-applicants)
+
+dat_tidy <- spread(dat, gender, admitted)
+dat_tidy
+
+# Question 13 Now use the admissions dataset to create the object tmp, which has columns major, gender, key and value:
+tmp <- gather(admissions, key, value, admitted:applicants)
+tmp
+ # Combine the key and gender and create a new column called column_name to get a variable with the following values: admitted_men, admitted_women, applicants_men and applicants_women. Save the new data as tmp2.
+tmp2 <- unite(tmp, column_name, c(key,gender))
+tmp2
+
+# Question 14 - Which function can reshape tmp2 to a table with six rows and five columns named major, admitted_men, admitted_women, applicants_men and applicants_women?
+spread(tmp2, column_name, value)
