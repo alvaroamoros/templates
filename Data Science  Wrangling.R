8d76dcf614c0fd48cf55f1a30e3f79c9ff68a622
alvaroamoros <100421520@alumnos.uc3m.es>
2020-10-11 16:15:56 +0200

    s

--- Data Science  Wrangling.R
+++ Data Science  Wrangling.R
@@ -22,7 +22,7 @@ filename - murders.csv
 fullpath - file.path(path, filename)
 fullpath
 
-file.copy(fullpath, getwd())  
+file.copy(fullpath, getwd())
 
 
 # The readr and readxl Packages
@@ -72,7 +72,7 @@ library(dslabs)
 data(gapminder)
 
 # Tidy data fomat
-tidy_data - gapminder %% 
+tidy_data - gapminder %%
   filter(country %in% c(South Korea, Germany)) %%
   select(country, year, fertility)
 head(tidy_data)
@@ -126,31 +126,31 @@ dat
       # 2. names to be used in the new columns
       # 3. the character that separates the variable
 
-      dat %>% separate(key, c("year", "variable_name"), "_") 
+      dat %>% separate(key, c("year", "variable_name"), "_")
       # We optain to many values
-   
-      dat %>% separate(key, c("year", "first_variable_name", "second_variable_name"), 
+
+      dat %>% separate(key, c("year", "first_variable_name", "second_variable_name"),
                        fill = "right")
-      
+
       # Argument extra, merges two variables if there is an extra separation
-      dat %>% separate(key, c("year", "variable_name"), sep = "_", extra = "merge") 
-      
+      dat %>% separate(key, c("year", "variable_name"), sep = "_", extra = "merge")
+
       dat %>% separate(key, c("year", "variable_name"), sep = "_", extra = "merge") %>%
         spread(variable_name, value)
-      
+
       # We can obtain the same result using the unite function
-      dat %>% 
+      dat %>%
         separate(key, c("year", "first_variable_name", "second_variable_name"), fill = "right") %>%
         unite(variable_name, first_variable_name, second_variable_name, sep="_")
-      
-      dat %>% 
+
+      dat %>%
         separate(key, c("year", "first_variable_name", "second_variable_name"), fill = "right") %>%
         unite(variable_name, first_variable_name, second_variable_name, sep="_") %>%
         spread(variable_name, value) %>%
         rename(fertility = fertility_NA)
-      
- 
-#Assessment Part 2: Reshaping Data  
+
+
+#Assessment Part 2: Reshaping Data
 =======
 # Separate - separates different elements of a name
 # Arguments (apart from the data)
@@ -158,40 +158,40 @@ dat
 # 2. names to be used in the new columns
 # 3. the character that separates the variable
 
-dat %% separate(key, c(year, variable_name), _) 
+dat %% separate(key, c(year, variable_name), _)
 # We optain to many values
 
-dat %% separate(key, c(year, first_variable_name, second_variable_name), 
+dat %% separate(key, c(year, first_variable_name, second_variable_name),
                 fill = right)
 
 # Argument extra, merges two variables if there is an extra separation
-dat %% separate(key, c(year, variable_name), sep = _, extra = merge) 
+dat %% separate(key, c(year, variable_name), sep = _, extra = merge)
 
 dat %% separate(key, c(year, variable_name), sep = _, extra = merge) %%
   spread(variable_name, value)
 
 # We can obtain the same result using the unite function
-dat %% 
+dat %%
   separate(key, c(year, first_variable_name, second_variable_name), fill = right) %%
   unite(variable_name, first_variable_name, second_variable_name, sep=_)
 
-dat %% 
+dat %%
   separate(key, c(year, first_variable_name, second_variable_name), fill = right) %%
   unite(variable_name, first_variable_name, second_variable_name, sep=_) %%
   spread(variable_name, value) %%
   rename(fertility = fertility_NA)
 
 
-#Assessment Part 2 Reshaping Data  
+#Assessment Part 2 Reshaping Data
 >>>>>>> 0689f598b7962923a708508ee9f30d29efd667f4
 library(tidyverse)
 library(dslabs)
 co2
-head(co2)    
+head(co2)
 
 <<<<<<< HEAD
 #Question 10 - Run the following command to define the co2_wide object:
-co2_wide <- data.frame(matrix(co2, ncol = 12, byrow = TRUE)) %>% 
+co2_wide <- data.frame(matrix(co2, ncol = 12, byrow = TRUE)) %>%
   setNames(1:12) %>%
   mutate(year = as.character(1959:1997))
 co2_wide
@@ -204,7 +204,7 @@ head(co2_tidy)
 co2_tidy %>% ggplot(aes(as.numeric(month), co2, color = year)) + geom_line()
 =======
 #Question 10 - Run the following command to define the co2_wide object
-co2_wide - data.frame(matrix(co2, ncol = 12, byrow = TRUE)) %% 
+co2_wide - data.frame(matrix(co2, ncol = 12, byrow = TRUE)) %%
   setNames(112) %%
   mutate(year = as.character(19591997))
 co2_wide
@@ -217,7 +217,7 @@ head(co2_tidy)
 co2_tidy %% ggplot(aes(as.numeric(month), co2, color = year)) + geom_line()
 >>>>>>> 0689f598b7962923a708508ee9f30d29efd667f4
 
-# Question 12 
+# Question 12
 library(dslabs)
 data(admissions)
 <<<<<<< HEAD
@@ -355,13 +355,10 @@ dat
 
 
 
-install.packages("Lahman")
-library(Lahman)
-
-# Question 5 - Use the correct join or bind function to create a combined table of the names and statistics of the top 10 home run (HR) hitters for 2016. 
+# Question 5 - Use the correct join or bind function to create a combined table of the names and statistics of the top 10 home run (HR) hitters for 2016.
 # This table should have the player ID, first name, last name, and number of HR for the top 10 players. Name this data frame top_names.
 
-top <- Batting %>% 
+top <- Batting %>%
   filter(yearID == 2016) %>%
   arrange(desc(HR)) %>%    # arrange by descending HR count
   slice(1:10)    # take entries 1-10
@@ -374,8 +371,8 @@ top_names <- top %>%  left_join(Master)%>%
   select(playerID, nameFirst, nameLast, HR)
 top_names
 
-# Question 6 Inspect the Salaries data frame. Filter this data frame to the 2016 salaries, 
-#then use the correct bind join function to add a salary column to the top_names data frame from the previous question. 
+# Question 6 Inspect the Salaries data frame. Filter this data frame to the 2016 salaries,
+#then use the correct bind join function to add a salary column to the top_names data frame from the previous question.
 #Name the new data frame top_salary. Use this code framework:
 
 top_salary <- Salaries %>% filter(yearID == 2016) %>%
@@ -383,7 +380,7 @@ top_salary <- Salaries %>% filter(yearID == 2016) %>%
   select(nameFirst, nameLast, teamID, HR, salary)
 top_salary
 
-# QUestion 7 - 
+# QUestion 7 -
 # Inspect the AwardsPlayers table. Filter awards to include only the year 2016.
 head(AwardsPlayers)
 awards_2016 <- AwardsPlayers %>% filter(yearID == 2016)
@@ -402,7 +399,6 @@ setdiff(awards_2016, top)
 
 
     # Web Scraping
-install.packages("rvest")
 library(rvest)
 url <- "https://en.wikipedia.org/wiki/Murder_in_the_United_States_by_state"
 h <- read_html(url)
@@ -410,7 +406,7 @@ class(h)
 h
 
 tab <- h %>% html_nodes("table")
-tab 
+tab
 
 tab <- tab %>% html_table
 class(tab)
@@ -434,7 +430,7 @@ get_recipe <- function(url){
   prep_time <- h %>% html_node("//li[(((count(preceding-sibling::*) + 1) = 1) and parent::*)]//strong") %>% html_text()
   ingredients <- h %>% html_nodes("//*[contains(concat( " ", @class, " " ), concat( " ", "ingredient", " " ))]") %>% html_text()
   return(list(recipe = recipe, prep_time = prep_time, ingredients = ingredients))
-} 
+}
 get_recipe("https://foodnetwork.co.uk/recipes/pancakes-4926/")
 
 
@@ -449,4 +445,357 @@ nodes <- html_nodes(h, "table")
 html_text(nodes[[8]])
 
 html_table(nodes[[8]])
->>>>>>> 0689f598b7962923a708508ee9f30d29efd667f4
+
+  # Section 3: String Processing
+url <- "https://en.wikipedia.org/w/index.php?title=Gun_violence_in_the_United_States_by_state&direction=prev&oldid=810166167"
+murders_raw <- read_html(url) %>%
+  html_nodes("table") %>%
+  html_table() %>%
+  .[[1]] %>%
+  setNames(c("state", "population", "total", "murder_rate"))
+murders_raw
+
+class(murders_raw$population)
+class(murders_raw$total)
+
+# to include both single and double quotes in string, escape with \
+s <- '5\'10"'
+s
+cat(s)
+
+s <- "5'10\""
+s
+cat(s)
+
+murders_raw$population[1:3]
+as.numeric(murders_raw$population[1:3])
+
+# detect whether there are commas
+commas <- function(x) any(str_detect(x, ","))
+murders_raw %>% summarize_all(funs(commas))
+
+# replace commas with the empty string and convert to numeric
+test_1 <- str_replace_all(murders_raw$population, ",", "")
+test_1 <- as.numeric(test_1)
+
+# parse_number also removes commas and converts to numeric
+test_2 <- parse_number(murders_raw$population)
+test_2
+
+murders_new <- murders_raw %>% mutate_at(2:3, parse_number)
+murders_new
+
+# Case Study 2: Reported Heights
+library(dslabs)
+data(reported_heights)
+class(reported_heights$height)
+
+x <- as.numeric(reported_heights$height)
+head(x)
+sum(is.na(x))
+
+# keep only entries that result in NAs
+reported_heights %>% mutate(new_height = as.numeric(height)) %>%
+  filter(is.na(new_height)) %>%
+  head()
+
+# calculate cutoffs that cover 99.999% of human population
+alpha <- 1/10^6
+qnorm(1-alpha/2, 69.1, 2.9)
+qnorm(alpha/2, 63.7, 2.7)
+
+# keep only entries that either result in NAs or are outside the plausible range of heights
+not_inches <- function(x, smallest = 50, tallest = 84){
+  inches <- suppressWarnings(as.numeric(x))
+  ind <- is.na(inches) | inches < smallest | inches > tallest
+  ind
+}
+
+# number of problematic entries
+problems <- reported_heights %>%
+  filter(not_inches(height)) %>%
+  .$height
+length(problems)
+
+# 10 examples of x'y or x'y" or x'y\"
+pattern <- "^\\d\\s*'\\s*\\d{1,2}\\.*\\d*'*\"*$"
+str_subset(problems, pattern) %>%
+  head %>%
+  cat
+
+# 10 examples of x.y or x,y
+pattern <- "^[4-6]\\s*[\\.|,]\\s*([0-9]|10|11)$"
+str_subset(problems, pattern) %>% head(n=10) %>% cat
+
+# 10 examples of entries in cm rather than inches
+ind <- which(between(suppressWarnings(as.numeric(problems))/2.54, 54, 81) )
+ind <- ind[!is.na(ind)]
+problems[ind] %>% head(n=10) %>% cat
+
+
+# Regex
+pattern <- ","
+str_detect(murders_raw$total, pattern)
+
+str_detect(reported_heights$height, "cm")
+
+yes <- c("180 cm", "70 inches")
+no <- c("180", "70''")
+s <- c(yes, no)
+
+str_detect(s, "cm") | str_detect(s, "inches")
+str_detect(s, "cm|inches")
+
+str_view(s, pattern)
+str_view_all(s, pattern)
+
+# Character Classes, Anchors and Quantifiers
+# s was defined in the previous video
+yes <- c("5", "6", "5'10", "5 feet", "4'11")
+no <- c("", ".", "Five", "six")
+s <- c(yes, no)
+pattern <- "\\d"
+
+str_view(s, "[56]")
+
+# [4-7] means 4, 5, 6 or 7
+yes <- as.character(4:7)
+no <- as.character(1:3)
+s <- c(yes, no)
+str_detect(s, "[4-7]")
+
+# ^ means start of string, $ means end of string
+pattern <- "^\\d$"
+yes <- c("1", "5", "9")
+no <- c("12", "123", " 1", "a4", "b")
+s <- c(yes, no)
+str_view(s, pattern)
+
+# curly braces define quantifiers: 1 or 2 digits
+pattern <- "^\\d{1,2}$"
+yes <- c("1", "5", "9", "12")
+no <- c("123", "a4", "b")
+str_view(c(yes, no), pattern)
+
+# combining character class, anchors and quantifier
+pattern <- "^[4-7]'\\d{1,2}\"$"
+yes <- c("5'7\"", "6'2\"",  "5'12\"")
+no <- c("6,2\"", "6.2\"","I am 5'11\"", "3'2\"", "64")
+str_detect(yes, pattern)
+str_detect(no, pattern)
+
+# Search and Replace with Regex
+# * means 0 or more instances of the previous character. ? means 0 or 1 instances. + means 1 or more instances.
+
+# number of entries matching our desired pattern
+pattern <- "^[4-7]'\\d{1,2}\"$"
+sum(str_detect(problems, pattern))
+
+# inspect examples of entries with problems
+problems[c(2, 10, 11, 12, 15)] %>% str_view(pattern)
+str_subset(problems, "inches")
+str_subset(problems, "''")
+
+# replace or remove feet/inches words before matching
+pattern <- "^[4-7]'\\d{1,2}$"
+problems %>%
+  str_replace("feet|ft|foot", "'") %>% # replace feet, ft, foot with '
+  str_replace("inches|in|''|\"", "") %>% # remove all inches symbols
+  str_detect(pattern) %>%
+  sum()
+
+# R does not ignore whitespace
+identical("Hi", "Hi ")
+
+# \\s represents whitespace
+pattern_2 <- "^[4-7]'\\s\\d{1,2}\"$"
+str_subset(problems, pattern_2)
+
+# * means 0 or more instances of a character
+yes <- c("AB", "A1B", "A11B", "A111B", "A1111B")
+no <- c("A2B", "A21B")
+str_detect(yes, "A1*B")
+str_detect(no, "A1*B")
+
+# test how *, ? and + differ
+data.frame(string = c("AB", "A1B", "A11B", "A111B", "A1111B"),
+           none_or_more = str_detect(yes, "A1*B"),
+           nore_or_once = str_detect(yes, "A1?B"),
+           once_or_more = str_detect(yes, "A1+B"))
+
+# update pattern by adding optional spaces before and after feet symbol
+pattern <- "^[4-7]\\s*'\\s*\\d{1,2}$"
+problems %>%
+  str_replace("feet|ft|foot", "'") %>% # replace feet, ft, foot with '
+  str_replace("inches|in|''|\"", "") %>% # remove all inches symbols
+  str_detect(pattern) %>%
+  sum()
+
+# Groups with Regex
+# define regex with and without groups
+pattern_without_groups <- "^[4-7],\\d*$"
+pattern_with_groups <-  "^([4-7]),(\\d*)$"
+
+# create examples
+yes <- c("5,9", "5,11", "6,", "6,1")
+no <- c("5'9", ",", "2,8", "6.1.1")
+s <- c(yes, no)
+
+# demonstrate the effect of groups
+str_detect(s, pattern_without_groups)
+str_detect(s, pattern_with_groups)
+
+# demonstrate difference between str_match and str_extract
+str_match(s, pattern_with_groups)
+str_extract(s, pattern_with_groups)
+
+# improve the pattern to recognize more events
+pattern_with_groups <-  "^([4-7]),(\\d*)$"
+yes <- c("5,9", "5,11", "6,", "6,1")
+no <- c("5'9", ",", "2,8", "6.1.1")
+s <- c(yes, no)
+str_replace(s, pattern_with_groups, "\\1'\\2")
+
+# Final pattern
+pattern_with_groups <-"^([4-7])\\s*[,\\.\\s+]\\s*(\\d*)$"
+
+# combine stringr commands with the pipe
+str_subset(problems, pattern_with_groups) %>% head
+str_subset(problems, pattern_with_groups) %>%
+  str_replace(pattern_with_groups, "\\1'\\2") %>% head
+
+#Testing and Improving
+
+# function to detect entries with problems
+not_inches_or_cm <- function(x, smallest = 50, tallest = 84){
+  inches <- suppressWarnings(as.numeric(x))
+  ind <- !is.na(inches) &
+    ((inches >= smallest & inches <= tallest) |
+       (inches/2.54 >= smallest & inches/2.54 <= tallest))
+  !ind
+}
+
+# identify entries with problems
+problems <- reported_heights %>%
+  filter(not_inches_or_cm(height)) %>%
+  .$height
+problems
+length(problems)
+
+converted <- problems %>%
+  str_replace("feet|foot|ft", "'") %>% #convert feet symbols to '
+  str_replace("inches|in|''|\"", "") %>%  #remove inches symbols
+  str_replace("^([4-7])\\s*[,\\.\\s+]\\s*(\\d*)$", "\\1'\\2") ##change format
+
+# find proportion of entries that fit the pattern after reformatting
+pattern <- "^[4-7]\\s*'\\s*\\d{1,2}$"
+index <- str_detect(converted, pattern)
+mean(index)
+
+converted[!index]
+
+
+
+problems <- c("5.3", "5,5", "6 1", "5 .11", "5, 12")
+pattern_with_groups <- "^([4-7])[,\\.\\s](\\d*)$"
+str_replace(problems, pattern_with_groups, "\\1'\\2")
+
+
+# Separate with Regex
+#The extract() function behaves similarly to the separate() function but allows extraction of groups from regular expressions.
+
+# First example - normally formatted heights
+s <- c("5'10","6'1")
+tab <- data.frame(x = s)
+tab
+
+# the separate and extract functions behave similarly
+tab %>% separate(x, c("feet", "inches"), sep = "'")
+
+tab %>% extract(x, c("feet", "inches"), regex = "(\\d)'(\\d{1,2})")
+tab
+
+# second example - some heights with unusual formats
+s <- c("5'10", "6'1\"", "5'8inches")
+s
+tab <- data.frame(x = s)
+tab
+
+# separate fails because it leaves in extra characters, but extract keeps only the digits because of regex groups
+tab %>% separate(x, c("feet", "inches"), sep = "'", fill = "right")
+
+tab %>% extract(x, c("feet", "inches"), regex = "(\\d)'(\\d{1,2})")
+
+#Using Groups and Quantifiers. Examples
+
+  #1 Many students measuring exactly 5 or 6 feet did not enter any inches. For example, 6' - our pattern requires that inches be included.
+  #2 Some students measuring exactly 5 or 6 feet entered just that number.
+  #3 Some of the inches were entered with decimal points. For example 5'7.5''. Our pattern only looks for two digits.
+  #4 Some entires have spaces at the end, for example 5 ' 9.
+  #5 Some entries are in meters and some of these use European decimals: 1.6, 1,7.
+  #6 Two students added cm.
+  #7 One student spelled out the numbers: Five foot eight inches.
+
+
+
+  # Case 1
+  yes <- c("5","6", "5")
+  no <- c("5'", "5''", "5'4")
+  s <- c(yes, no)
+
+  # Case 2
+  str_replace(s, "^([56])'?$", "\\1'0")
+
+  # case 2.2
+  str_replace(s, "^([56])'*$", "\\1'0")
+
+  #Case 3
+  pattern <- "^[4-7]\\s*'\\s*(\\d+\\.?\\d*)$"
+
+  # case 5
+  yes <- c("1,7", "1, 8", "2, ")
+  no <- c("5,8", "5,3,2", "1.7")
+  s <-c(yes, no)
+  s
+  str_replace(s, "^([12])\\s*,\\s*(\\d*)$", "\\1\\.\\2")
+
+  # Trimming <- remove spaces from start and end of strings
+  str_trim("5 ' 9 ")
+
+  # str_to_lower <- Removes ower case letters
+  s <- c("Five feet eight inches")
+  str_to_lower(s)
+
+# Putting it into a function
+  convert_format <- function(s){
+    s %>%
+      str_replace("feet|foot|ft", "'") %>% #convert feet symbols to '
+      str_replace_all("inches|in|''|\"|cm|and", "") %>%  #remove inches and other symbols
+      str_replace("^([4-7])\\s*[,\\.\\s+]\\s*(\\d*)$", "\\1'\\2") %>% #change x.y, x,y x y
+      str_replace("^([56])'?$", "\\1'0") %>% #add 0 when to 5 or 6
+      str_replace("^([12])\\s*,\\s*(\\d*)$", "\\1\\.\\2") %>% #change european decimal
+      str_trim() #remove extra space
+  }
+
+  words_to_numbers <- function(s){
+    str_to_lower(s) %>%
+      str_replace_all("zero", "0") %>%
+      str_replace_all("one", "1") %>%
+      str_replace_all("two", "2") %>%
+      str_replace_all("three", "3") %>%
+      str_replace_all("four", "4") %>%
+      str_replace_all("five", "5") %>%
+      str_replace_all("six", "6") %>%
+      str_replace_all("seven", "7") %>%
+      str_replace_all("eight", "8") %>%
+      str_replace_all("nine", "9") %>%
+      str_replace_all("ten", "10") %>%
+      str_replace_all("eleven", "11")
+  }
+
+  # Now we can see which problematic entries remain:
+  converted <- problems %>% words_to_numbers %>% convert_format
+  remaining_problems <- converted[not_inches_or_cm(converted)]
+  pattern <- "^[4-7]\\s*'\\s*\\d+\\.?\\d*$"
+  index <- str_detect(remaining_problems, pattern)
+  remaining_problems[!index]
