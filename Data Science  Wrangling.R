bbddc44df7792f7d3aaf4a977f90875cfef1a3a8
alvaroamoros <100421520@alumnos.uc3m.es>
2020-10-16 12:08:12 +0200

    s

--- Data Science  Wrangling.R
+++ Data Science  Wrangling.R
@@ -151,21 +151,21 @@ dat %>%
 
 
 #Assessment Part 2: Reshaping Data
-=======
+
   # Separate - separates different elements of a name
   # Arguments (apart from the data)
   # 1. name of the column to be separated
   # 2. names to be used in the new columns
   # 3. the character that separates the variable
   
-  dat %% separate(key, c(year, variable_name), _)
+  dat %% separate(key, c(year, variable_name), )
 # We optain to many values
 
 dat %% separate(key, c(year, first_variable_name, second_variable_name),
                 fill = right)
 
 # Argument extra, merges two variables if there is an extra separation
-dat %% separate(key, c(year, variable_name), sep = _, extra = merge)
+dat %% separate(key, c(year, variable_name), sep = , extra = merge)
 
 dat %% separate(key, c(year, variable_name), sep = _, extra = merge) %%
   spread(variable_name, value)
@@ -1052,3 +1052,235 @@ library(lubridate)
 library(tidyr)
 library(scales)
 set.seed(1)
+
+
+  #Case study: Trump tweets
+
+url <- 'http://www.trumptwitterarchive.com/data/realdonaldtrump/%s.json'
+trump_tweets <- map(2009:2017, ~sprintf(url, .x)) %>%
+  map_df(jsonlite::fromJSON, simplifyDataFrame = TRUE) %>%
+  filter(!is_retweet & !str_detect(text, '^"')) %>%
+  mutate(created_at = parse_date_time(created_at, 
+                                      orders = "a b! d! H!:M!:S! z!* Y!",
+                                      tz="EST")) 
+library(dslabs)
+data("trump_tweets")
+head(trump_tweets)
+names(trump_tweets)
+?trump_tweets
+
+trump_tweets$text[16413] %>% str_wrap(width = options()$width) %>% cat
+
+trump_tweets %>% count(source) %>% arrange(desc(n)) %>% head()
+
+#We can use extract to remove the Twitter for part of the source and filter out retweets.
+trump_tweets %>%
+  extract(source, "source", "Twitter for (.*)") %>%
+  count(source)
+  
+#We are interested in what happened during the campaign
+campaign_tweets <- trump_tweets %>% 
+  extract(source, "source", "Twitter for (.*)") %>%
+  filter(source %in% c("Android", "iPhone") &
+           created_at >= ymd("2015-06-17") & 
+           created_at < ymd("2016-11-08")) %>%
+  filter(!is_retweet) %>%
+  arrange(created_at)
+
+# Data visualization to see the our of the day that tweets hapend
+ds_theme_set()
+campaign_tweets %>%
+  mutate(hour = hour(with_tz(created_at, "EST"))) %>%
+  count(source, hour) %>%
+  group_by(source) %>%
+  mutate(percent = n / sum(n)) %>%
+  ungroup %>%
+  ggplot(aes(hour, percent, color = source)) +
+  geom_line() +
+  geom_point() +
+  scale_y_continuous() +
+  labs(x = "Hour of day (EST)",
+       y = "% of tweets",
+       color = "")
+
+# Text as data 
+
+# The tidytext package helps us convert free from text into a tidy table. Having the data in this format greatly facilitates data visualization and applying statistical techniques.
+library(tidytext)
+
+example <- data_frame(line = c(1, 2, 3, 4),
+                      text = c("Roses are red,", "Violets are blue,", "Sugar is sweet,", "And so are you."))
+example
+example %>% unnest_tokens(word, text)
+
+# Now let's look at a quick example with a tweet number 3008:
+i <- 3008
+campaign_tweets$text[i]
+campaign_tweets[i,] %>% 
+  unnest_tokens(word, text) %>%
+  select(word)
+
+# Instead of using the default token, words, we define a regex that captures twitter character.
+pattern <- "([^A-Za-z\\d#@']|'(?![A-Za-z\\d#@]))"
+  #We can now use the unnest_tokens() function with the regex option and appropriately extract the hashtags and mentions:
+  campaign_tweets[i,] %>%
+    unnest_tokens(word, text, token = "regex", pattern = pattern) %>%
+    select(word)
+
+    # Another minor adjustment we want to make is remove the links to pictures:
+  campaign_tweets[i,] %>% 
+    mutate(text = str_replace_all(text, "https://t.co/[A-Za-z\\d]+|&amp;", ""))  %>%
+    unnest_tokens(word, text, token = "regex", pattern = pattern) %>%
+    select(word)
+      
+  tweet_words <- campaign_tweets %>% 
+    mutate(text = str_replace_all(text, "https://t.co/[A-Za-z\\d]+|&amp;", ""))  %>%
+    unnest_tokens(word, text, token = "regex", pattern = pattern) 
+tweet_words  
+
+# And we can now answer questions such as "what are the most commonly used words?"
+tweet_words %>%
+  count(word) %>%
+  arrange(desc(n))
+
+# The top words are not informative. The tidytext package has database of these commonly used words, referred to as stop words, in text mining:
+  # stop_words
+tweet_words <- campaign_tweets %>% 
+  mutate(text = str_replace_all(text, "https://t.co/[A-Za-z\\d]+|&amp;", ""))  %>%
+  unnest_tokens(word, text, token = "regex", pattern = pattern)  %>%
+  filter(!word %in% stop_words$word)
+
+tweet_words %>%
+  count(word) %>%
+  arrange(desc(n)) %>%
+  head(10)
+
+# Remove numbers and the quote '. We want to remove the ' 
+tweet_words <- campaign_tweets %>%
+  mutate(text = str_replace_all(text, "https://t.co/[A-Za-z\\d]+|&amp;", "")) %>%
+  unnest_tokens(word, text, token = "regex", pattern = pattern) %>%
+  filter(!word %in% stop_words$word &
+           !str_detect(word, "^\\d+$")) %>%
+  mutate(str_replace(word, "^'",""))
+
+tweet_words
+
+#Now that we have all our words in a table, along with information about what device was used to compose the tweet they came from
+# WE use odds ratio to do so
+
+android_iphone_or <- tweet_words %>%
+  count(word, source) %>%
+  spread(source, n, fill = 0) %>%
+  mutate( or = (Android + 0.5 / (sum(Android) - Android + 0.5)/
+                  ((iPhone + 0.5) / sum(iPhone) - iPhone + 0.5)))
+android_iphone_or %>% arrange(desc(or))
+
+android_iphone_or <- tweet_words %>%
+  count(word, source) %>%
+  spread(source, n, fill = 0) %>%
+  mutate(or = (Android + 0.5) / (sum(Android) - Android + 0.5) / 
+           ( (iPhone + 0.5) / (sum(iPhone) - iPhone + 0.5)))
+
+android_iphone_or %>% arrange(desc(or))
+android_iphone_or %>% arrange(or) 
+
+# Given that several of these words are overall low frequency words we can impose a filter based on the total frequency like this:
+android_iphone_or %>% filter(Android + iPhone > 100) %>%
+  arrange(desc(or))
+android_iphone_or %>% filter(Android + iPhone > 100) %>%
+  arrange(or)
+
+#Sentiment Analysis
+
+view(sentiments)
+
+get_sentiments("bing")
+get_sentiments("afinn")
+get_sentiments("loughran") %>% count(sentiment)
+get_sentiments("nrc") %>% count(sentiment)
+
+nrc <- get_sentiments("nrc") %>%
+  select(word, sentiment)
+
+tweet_words %>% inner_join(nrc, by = "word") %>%
+  select(source, word, sentiment) %>% sample_n(10)
+  
+sentiment_counts <- tweet_words %>%
+  inner_join(nrc, by = "word") %>%
+  count(source, sentiment) %>%
+  spread(source, n) %>%
+  mutate( sentiment = replace_na(sentiment, replace = "none"))
+sentiment_counts
+
+tweet_words %>% group_by(source) %>% summarise(n = n())
+
+sentiment_counts %>%
+  mutate(Android = Android / (sum(Android) - Android) , 
+         iPhone = iPhone / (sum(iPhone) - iPhone), 
+         or = Android/iPhone) %>%
+  arrange(desc(or))
+
+# odds ratio and conficende interval
+
+library(broom)
+log_or <- sentiment_counts %>%
+  mutate( log_or = log( (Android / (sum(Android) - Android)) / (iPhone / (sum(iPhone) - iPhone))),
+          se = sqrt( 1/Android + 1/(sum(Android) - Android) + 1/iPhone + 1/(sum(iPhone) - iPhone)),
+          conf.low = log_or - qnorm(0.975)*se,
+          conf.high = log_or + qnorm(0.975)*se) %>%
+  arrange(desc(log_or))          
+log_or
+
+# Graphical representation
+log_or %>%
+  mutate(sentiment = reorder(sentiment, log_or),) %>%
+  ggplot(aes(x = sentiment, ymin = conf.low, ymax = conf.high)) +
+  geom_errorbar() +
+  geom_point(aes(sentiment, log_or)) +
+  ylab("Log odds ratio for association between Android and sentiment") +
+  coord_flip() 
+
+
+android_iphone_or %>% inner_join(nrc) %>%
+  filter(sentiment == "disgust" & Android + iPhone > 10) %>%
+  arrange(desc(or))
+  
+android_iphone_or %>% inner_join(nrc, by = "word") %>%
+  mutate(sentiment = factor(sentiment, levels = log_or$sentiment)) %>%
+  mutate(log_or = log(or)) %>%
+  filter(Android + iPhone > 10 & abs(log_or)>1) %>%
+  mutate(word = reorder(word, log_or)) %>%
+  ggplot(aes(word, log_or, fill = log_or < 0)) +
+  facet_wrap(~sentiment, scales = "free_x", nrow = 2) + 
+  geom_bar(stat="identity", show.legend = FALSE) +
+  theme(axis.text.x = element_text(angle = 90, hjust = 1)) 
+
+
+# Assessment Part 1: Dates, Times, and Text Mining
+library(dslabs)
+library(lubridate)
+options(digits = 3) 
+
+data("brexit_polls")
+
+# Question 1 
+  #How many polls had a start date (startdate) in April (month number 4)?
+brexit_polls %>% mutate(month = month(startdate)) %>% filter(month == 4)
+
+
+#Use the round_date() function on the enddate column with the argument unit="week". How many polls ended the week of 2016-06-12?
+brexit_polls %>% mutate(month_end = month(enddate),
+                        day_end = day(enddate))
+
+brexit_polls$enddate
+
+round_brexit_polls <- round_date(brexit_polls$enddate, unit = "week")
+round_brexit_polls <- data.frame(enddate = round_brexit_polls) %>%
+  mutate(month = month(enddate),
+         day = day(enddate))
+round_brexit_polls %>% filter(month == 6,
+                              day == 12)
+
+week_days <- weekdays(brexit_polls$enddate)
+week_days <- data.frame(day = week_days) 
+table(week_days$day)
